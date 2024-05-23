9914c55821141af904269d780f4e2c5b2a18303c
huncho-hackz <davart776@gmail.com>
2024-05-23 11:35:28 +0000

    Array cardio day 1 done and dusted. Need to wrap my hands around it to get it very well

--- day 4/Arraycardio.js
+++ day 4/Arraycardio.js
@@ -6,32 +6,37 @@ const inventors = [
     {firstName: 'Nicolaus', lastName: 'Copernicus', year: 1473, passed: 1553}
 ]
 const people = ['David, Arthur', 'Bernard, Adomah', 'Kwame, Adom', 'David, Shalom', 'Desmond, Dornu'];
+
 //Array.filter()
 const years = inventors.filter(inventor=> inventor.year > 1500 && inventor.year < 1600);
-// console.log(years);
+console.log(years);
+
 //Array.map()
 const names = inventors.map(inventor => inventor.firstName + ' ' + inventor.lastName);
-// console.log(names);
+console.log(names);
+
 //Array.sort();
 const sort = inventors.sort((a,b)=> a.year > b.year ? 1 : -1);
-// console.table(sort);
+console.table(sort);
+
 //Array.reduce()
 const reduce = inventors.reduce((sum, inventor)=> sum + inventor.passed - inventor.year,0);
-// console.log(reduce);
+console.log(reduce);
+
 //Sorting the oldest person
 const oldest = inventors.sort((a, b)=>{
     const lastGuy = a.passed - a.year;
     const nextGuy = b.passed - b.year;
     return lastGuy > nextGuy ? -1 : 1;
 })
-// console.table(oldest);
+console.table(oldest);
 
 //filtering in a category in https://wikipedia.org/wiki/Category:Boulevards_in_Paris
-// const category = document.querySelector('.mw-category');
-// const Names = [...category.querySelectorAll('a')];
-// const de = Names.map(Name=> Name.textContent)
-//                 .filter(streetNames => streetNames.includes('de'));
-// // console.log(de);
+const category = document.querySelector('.mw-category');
+const Names = [...category.querySelectorAll('a')];
+const de = Names.map(Name=> Name.textContent)
+                .filter(streetNames => streetNames.includes('de'));
+console.log(de);
 
 const pep = people.sort((lastOne, firstOne)=>{
     const [aLast, aFirst] = lastOne.split(', ');
@@ -41,3 +46,14 @@ const pep = people.sort((lastOne, firstOne)=>{
 }
 );
 console.log(pep);
+
+// reduce this data
+const data = ['car', 'truck', 'bike', 'bike', 'car', 'truck', 'truck', 'bike', 'car', 'car', 'truck'];
+const transportation = data.reduce((obj, item)=>{
+     if(!obj[item]){
+        obj[item]=0;
+     };
+     obj[item]++;
+     return obj;
+}, {})
+console.log(transportation);
