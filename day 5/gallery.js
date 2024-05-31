58d38669a02057cab67aa600251afa8eee370c86
huncho-hackz <davart776@gmail.com>
2024-05-31 12:00:02 +0000

    done with flex panel

--- day 5/gallery.js
+++ day 5/gallery.js
@@ -3,4 +3,11 @@ const panels = document.querySelectorAll('.panel');
 function toggleOpen(){
     this.classList.toggle('open');
 }
+function transition(e){
+    if(e.propertyName.includes('flex'))
+        {
+            this.classList.toggle('open-active');
+        }
+}
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel=> panel.addEventListener('transitionend', transition));
