4f017c2159153665fc8298c2dc1dc20fd46d0a51
huncho-hackz <davart776@gmail.com>
2024-06-03 16:33:24 +0000

    learning the fetch method in js

---
+++ day 6/ajax.js
@@ -0,0 +1,11 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+let cities = [];
+const prom = fetch(endpoint)
+            .then(blob => blob.json())
+            .then(data => cities = data);
+function findMatch(wordToMatch, cities){
+    cities.filter(place => {
+        return 
+    })
+
+}
