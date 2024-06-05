683de937e3c7250c00197e931b76c0e6ef266de9
huncho-hackz <davart776@gmail.com>
2024-06-05 18:13:26 +0000

    ajax

--- day 6/ajax.js
+++ day 6/ajax.js
@@ -1,13 +1,31 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
-const prom = fetch(endpoint)
-            .then(blob => blob.json())
-            .then(data => cities.push(...data));
+fetch(endpoint)
+        .then(blob => blob.json())
+        .then(data => cities.push(...data))
+        .catch(error => console.error('Fetch error', error));
 
 function findMatch(wordToMatch, cities){
-    cities.filter(place => {
-        const regEx = new RegExp(wordToMatch, 'gi')
+  return  cities.filter(place => {
+        const regEx = new RegExp(wordToMatch, 'gi');
         return place.city.match(regEx) || place.state.match(regEx);
-    })
-
+    });
 }
+
+const search = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions')
+function displayMatches(){
+    const matchArray = findMatch(this.value, cities);
+    const html = matchArray.map(place => {
+        return `
+        <li>
+            <span class="name">${place.city}, ${place.state}}</span>
+            <span class="population>${place.population}</span>
+        </li>
+        `
+    });
+    suggestions.innerHTML = html;
+}
+
+search.addEventListener('change', displayMatches);
+search.addEventListener('keyup', displayMatches);
